3d19b7c74cdb5e3dd0d81d68a789c347829371ab
Heecheon Park <heecheon92@gmail.com>
2023-10-09 21:45:56 +0900

    base project section covered

---
+++ src/components/Projects.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import { motion } from "framer-motion";
+
+export default function Projects() {
+  const projects = [1, 2, 3, 4, 5];
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1.5 }}
+      className="relative z-0 mx-auto flex h-screen max-w-full flex-col items-center justify-evenly overflow-hidden text-left md:flex-row"
+    >
+      <h3 className="absolute top-24 text-2xl uppercase tracking-[20px] text-gray-500">
+        Projects
+      </h3>
+
+      <div className="relative z-20 flex w-full snap-x snap-mandatory overflow-y-hidden overflow-x-scroll">
+        {projects.map((project, idx) => (
+          <div
+            key={idx}
+            className="flex h-screen w-screen flex-shrink-0 snap-center flex-col items-center justify-center space-y-5 p-20 md:p-44"
+          >
+            <motion.img
+              initial={{ y: -300, opacity: 0 }}
+              transition={{ duration: 1.2 }}
+              whileInView={{ y: 0, opacity: 1 }}
+              viewport={{ once: true }}
+              src="https://placehold.co/600x400.png"
+              alt="placeholder"
+            />
+
+            <div className="max-w-6xl space-y-10 px-0 md:px-10">
+              <h4 className="text-center text-4xl font-semibold">
+                <span className="underline decoration-[#F7AB0A]/50">
+                  Case Study {idx + 1} of {projects.length}:
+                </span>{" "}
+                Some Project
+              </h4>
+            </div>
+
+            <p className="tex-center text-lg md:text-left">
+              {`Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum. Contrary to popular belief, Lorem
+          Ipsum is not simply random text.`}
+            </p>
+          </div>
+        ))}
+      </div>
+
+      <div className="absolute left-0 top-[30%] h-[500px] w-full -skew-y-12 bg-[#F7AB0A]/10" />
+    </motion.div>
+  );
+}
